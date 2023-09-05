1cdbff6dc5b446839d886358ba3ab98e2280e715
Kusum <kusumbanerjee.18@gmail.com>
2023-09-05 14:08:28 +0530

    editing in bdetail page

--- components/bDetail.vue
+++ components/bDetail.vue
@@ -1,18 +1,18 @@
 <template>
   <!-- img section -->
-  <div class="flex flex-col sm:flex-row gap-7 justify-items-center m-10">
+  <div class="flex flex-col sm:flex-row justify-items-center m-10">
     <img class="w-full h-96 sm:w-64 sm:h-64 sm:object-cover" :src="image" />
 
-    <div class="bg-white round-lg shadow-md p-4 sm:ml-4">
-      <h1 class="font-bold text-2xl mb-3">{{ title }}</h1>
-      <h2 class="font-semibold text-lg mb-3">{{ author }}</h2>
+    <div class="bg-white round-lg shadow-md p-2 sm:ml-4">
+      <h1 class="font-bold text-2xl mb-2">{{ title }}</h1>
+      <h2 class="font-semibold text-lg mb-2">{{ author }}</h2>
       <h3 class="text-gray-500 text-md mb-2">{{ des }}</h3>
     </div>
   </div>
 </template>
 
 <script setup>
-import { defineProps } from "vue";
+// import { defineProps } from "vue";
 
 defineProps(["ISBN", "title", "image", "author", "des"]);
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -10,7 +10,12 @@
       <!-- passing the const data -->
 
       <div v-for="book in data.items" :key="book.id">
-        <BCard :bookid="book.id" :ISBN="book.volumeInfo.industryIdentifiers[0].identifier" :title="book.volumeInfo.title" :image="book.volumeInfo.imageLinks.thumbnail" />
+        <BCard
+          :bookid="book.id"
+          :ISBN="book.volumeInfo.industryIdentifiers[0].identifier"
+          :title="book.volumeInfo.title"
+          :image="book.volumeInfo.imageLinks.thumbnail"
+        />
       </div>
     </div>
   </div>
@@ -22,8 +27,7 @@ const { data: data } = await useFetch(
 );
 </script>
 
-
- <!-- <!- google api -->
+<!-- <!- google api -->
 <!-- https://www.googleapis.com/books/v1/volumes?q=pride+prejudice&download=epub&key=AIzaSyB_365A3v-S93S4h05_3_gt5PJEL1pHFZw -->
 <!-- api openlibrary
  const {data: books,} = await useFetch("https://openlibrary.org/api/books?bibkeys=ISBN:0451526538&callback=mycallback")
