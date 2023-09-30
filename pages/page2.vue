cdab3099b503d809c30d5d45814cf088e734bc30
Kusum <kusumbanerjee.18@gmail.com>
2023-09-30 14:44:04 +0530

    continue... pagination

---
+++ pages/page2.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h1>Explore Mystery Shelf</h1>
+    <Pagination
+      :currentPage="currentPage"
+      :totalPages="totalPages"
+      :prevPage="prevPage"
+      :nextPage="nextPage"
+    />
+
+    <div v-for="book in data.items" :key="book.id">
+      <BCard
+        :bookid="book.id"
+        :title="book.volumeInfo.title"
+        :image="book.volumeInfo.imageLinks?.thumbnail"
+      />
+    </div>
+  </div>
+</template>
+
+<script setup>
+onBeforeMount(() => {
+  const page = parseInt(route.params.page);
+  if (!isNaN(page) && page >= 1 && page <= totalPages) {
+    currentPage.value = page;
+  } else {
+    currentPage.value = 2; // Set 2 for page2, 3 for 3...so on
+  }
+});
+
+const { data } = await useFetch(
+  "https://www.googleapis.com/books/v1/volumes?q=mystery+inauthor:keyes&maxResults=40&key=AIzaSyAlTgCaAkO75NYmltCt8UwRvu6VNZXirv0" // Replace with your API key
+);
+</script>
