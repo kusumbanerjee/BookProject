5a55154e6ec839e3e47d155e475c794261c5d77f
Kusum <kusumbanerjee.18@gmail.com>
2023-09-30 22:28:33 +0530

    some changes

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,5 +1,5 @@
-import { createClient } from '@supabase/supabase-js'
+// import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://mwvfrygpulifqwgdktgt.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im13dmZyeWdwdWxpZnF3Z2RrdGd0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTU4MzUyMDEsImV4cCI6MjAxMTQxMTIwMX0.oYzfHH3yb8RKff10J4sz9cBY2N0VxN1zi2sbUHkhH9c'
-export const supabase = createClient(supabaseUrl, supabaseKey)
+// const supabaseUrl = 'https://mwvfrygpulifqwgdktgt.supabase.co'
+// const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im13dmZyeWdwdWxpZnF3Z2RrdGd0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTU4MzUyMDEsImV4cCI6MjAxMTQxMTIwMX0.oYzfHH3yb8RKff10J4sz9cBY2N0VxN1zi2sbUHkhH9c'
+// export const supabase = createClient(supabaseUrl, supabaseKey) 
